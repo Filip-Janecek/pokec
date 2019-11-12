789badda8b81d70b135340b2b0beaf13526bde5e
Filip-Janecek <filip.janecek28@gmail.com>
2019-11-12 17:06:14 +0100

    l

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const createSpaServer = require("spaserver").createSpaServer;
-const dateFormat = require('dateformat');
+const apiChat = require('./api-chat').apiChat;
 
 const PORT = 8080; //aplikace na Rosti.cz musi bezet na portu 8080
 const API_HEAD = {
@@ -9,20 +9,12 @@ const API_HEAD = {
 const API_STATUS_OK = 0;
 const API_STATUS_NOT_FOUND = -1;
 
-let list = new Array();
-
 function processApi(req, res) {
     res.writeHead(200, API_HEAD);
     let obj = {};
     obj.status = API_STATUS_OK;
-    if (req.pathname === "/list") {
-        obj.list = list;
-    } else if (req.pathname === "/add") {
-        let addObj = {};
-        let dt = new Date();
-        addObj.time = dateFormat(dt, "HH.MM.ss");
-        addObj.val = req.parameters.value;
-        list.push(addObj);
+    if (req.pathname.startsWith("/chat/")) {
+        apiChat(req, res, obj);
     } else {
         obj.status = API_STATUS_NOT_FOUND;
         obj.error = "API not found";
